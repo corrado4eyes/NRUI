2c8ec918f64f6a953a49dbc5847c478265d98040
Corrado Quattrocchi <corrado.quattrocchi.dev@gmail.com>
2025-09-08 15:32:03 +0200

    Remove PublishingType from ui module

--- ui/src/jvmMain/kotlin/PublishingType.kt
+++
@@ -1,9 +0,0 @@
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-enum class PublishingType {
-    @SerialName("automatic") AUTOMATIC,
-    @SerialName("user_managed") USER_MANAGED,
-    @SerialName("portal_api") PORTAL_API;
-}
