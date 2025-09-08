2b0b1e06e53dc0614694a6cff3f037c4838c0b38
Corrado Quattrocchi <corrado.quattrocchi.dev@gmail.com>
2025-09-08 12:30:44 +0200

    Add kotlin serialization and models

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -1,6 +1,4 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -17,8 +15,8 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-//            implementation(libs.ktor.client.)
         }
+
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
